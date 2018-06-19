08021e915a26b43e0a0048277b4e1a0c7bd731c9
citrus09 <citrus@cit.com>
2018-06-19 07:50:43 +0530

    with q learning agent

--- projects/smartcab/smartcab/agent.py
+++ projects/smartcab/smartcab/agent.py
@@ -39,6 +39,12 @@ class LearningAgent(Agent):
         # Update epsilon using a decay function of your choice
         # Update additional class parameters as needed
         # If 'testing' is True, set epsilon and alpha to 0
+        if testing:
+            self.epsilon = .0
+            self.alpha = .0
+        else:
+            self.epsilon = self.epsilon - .05
+
 
         return None
 
@@ -90,6 +96,9 @@ class LearningAgent(Agent):
         # When learning, check if the 'state' is not in the Q-table
         # If it is not, create a new dictionary for that state
         #   Then, for each action available, set the initial Q-value to 0.0
+        if self.learning:
+            if not (self.Q.has_key(state) == False):
+                self.Q[state] = {action:0.0 for action in self.env.valid_actions}
 
         return
 
@@ -153,7 +162,7 @@ def run():
     #   verbose     - set to True to display additional output from the simulation
     #   num_dummies - discrete number of dummy agents in the environment, default is 100
     #   grid_size   - discrete number of intersections (columns, rows), default is (8, 6)
-    env = Environment()
+    env = Environment(verbose=True)
     
     ##############
     # Create the driving agent
@@ -161,12 +170,13 @@ def run():
     #   learning   - set to True to force the driving agent to use Q-learning
     #    * epsilon - continuous value for the exploration factor, default is 1
     #    * alpha   - continuous value for the learning rate, default is 0.5
-    agent = env.create_agent(LearningAgent)
+    agent = env.create_agent(LearningAgent, learning=True)
     
     ##############
     # Follow the driving agent
     # Flags:
     #   enforce_deadline - set to True to enforce a deadline metric
+    # env.set_primary_agent(agent, enforce_deadline=True)
     env.set_primary_agent(agent, enforce_deadline=True)
 
     ##############
@@ -176,13 +186,15 @@ def run():
     #   display      - set to False to disable the GUI if PyGame is enabled
     #   log_metrics  - set to True to log trial and simulation results to /logs
     #   optimized    - set to True to change the default log file name
-    sim = Simulator(env, update_delay=3, log_metrics=True)
-    
+    # sim = Simulator(env, update_delay=3, log_metrics=True)
+    sim = Simulator(env, update_delay=.01, log_metrics=True)
+
     ##############
     # Run the simulator
     # Flags:
     #   tolerance  - epsilon tolerance before beginning testing, default is 0.05 
     #   n_test     - discrete number of testing trials to perform, default is 0
+    # sim.run(n_test=10)
     sim.run(n_test=10)
 
 
